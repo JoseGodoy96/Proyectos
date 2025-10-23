def2af5f5b82a4d1f1694920a9d6ec170765a808
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-23 11:11:57 +0200

    <fix>(reservas): Cambiar reservas para utilizar el middleware verificarToken

--- Proyectogrado/gimnasio-backend/routes/reservas.mjs
+++ Proyectogrado/gimnasio-backend/routes/reservas.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
 import { db } from '../db.mjs';
+import { verificarToken } from '../middleware/middlewareAuth.mjs';
 
 const router = express.Router();
 
@@ -11,56 +12,57 @@ router.get("/", (req, res) => {
 	});
 });
 
-router.get("/:id", (req, res) => {
-	const { id } = req.params;
-	db.query("SELECT id, usuario_id, clase_id, estado FROM reservas WHERE id = ?", 
-		[id],
-		(err, result) => {
-		if (err)
-			return res.status(500).json({ error: err.message });
-		if (result.length === 0)
-			return res.status(404).json({ error: "Reserva no encontrada" });
-		res.json(result[0]);
-	});
-});
-
-router.post("/", (req, res) => {
-	const { usuario_id, clase_id, estado = "pendiente" } = req.body;
-	if (!usuario_id || !clase_id)
-		return res.status(400).json({ error: "usuario_id y clase_id son obligatorios"});
-	db.query("INSERT INTO reservas (usuario_id, clase_id, estado) VALUES (?, ?, ?)", 
-		[usuario_id, clase_id, estado],
+router.get("/mis-reservas", verificarToken, (req, res) => {
+	const usuarioId = req.usuario.id;
+	db.query("SELECT id, clase_id, estado FROM reservas WHERE usuario_id = ?",
+		[usuarioId],
 		(err, result) => {
 			if (err)
 				return res.status(500).json({ error: err.message });
-			res.status(201).json({ id: result.insertId, usuario_id, clase_id, estado });
+			res.json(result);
 	});
 });
 
-router.put("/:id", (req, res) => {
+router.post("/", verificarToken, (req, res) => {
+	const { clase_id } = req.body;
+	const usuario_id = req.usuario.id;
+	if (!clase_id)
+		return res.status(400).json({ error: "clase_id es obligatorio" });
+	db.query("INSERT INTO reservas (usuario_id, clase_id, estado) VALUES (?, ?, 'pendiente')",
+		[usuario_id, clase_id],
+		(err, result) => {
+			if (err)
+				return res.status(500).json({ error: err.message });
+			res.status(201).json({ id: result.insertId, usuario_id, clase_id, estado: "pendiente"});
+	});
+});
+
+router.put("/:id", verificarToken, (req, res) => {
 	const { id } = req.params;
 	const { estado } = req.body;
-	db.query("UPDATE reservas SET estado = ? WHERE id = ?",
-		[estado, id],
+	const usuario_id = req.usuario.id;
+	db.query("UPDATE reservas SET estado = ? WHERE id = ? AND usuario_id = ?",
+		[estado, id, usuario_id],
 		(err, result) => {
 			if (err)
 				return res.status(500).json({ error: err.message });
 			if (result.affectedRows === 0)
-				return res.status(404).json({ error: "Reserva no encontrada" });
-			res.json({ message: "reserva actualizada"});
-		});
+				return res.status(404).json({ error: "Reserva no encontrada o no autorizada" });
+			res.json({ message: "Reserva actualizada" });
+	});
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", verificarToken, (req, res) => {
 	const { id } = req.params;
-	db.query("DELETE FROM reservas WHERE id = ?",
-		[id],
+	const usuario_id = req.usuario.id;
+	db.query("DELETE FROM reservas WHERE id = ? AND usuario_id = ?",
+		[id, usuario_id],
 		(err, result) => {
 			if (err)
 				return res.status(500).json({ error: err.message });
 			if (result.affectedRows === 0)
-				return res.status(404).json({ error: "Reserva no encontrada" });
-			res.json({ message: "Reserva eliminada"});
+				return res.status(404).json({ error: "Reserva no encontrada o no autorizada "});
+			res.json({ message: "Reserva eliminada" });
 	});
 });
 
